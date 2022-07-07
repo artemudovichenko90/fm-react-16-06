d23b669bfcbe690e3a539c4e6cd8dbd05b375258
Artem Udovichenko <artemudovichenko90@gmail.com>
2022-07-07 22:42:17 +0300

    Add UserSection
    State, Lifting State Up

---
+++ src/components/UserSection/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import SelectedUsers from './SelectedUsers/index';
+import UserList from './UserList/index';
+const userDB = [
+  { id: 1, fname: 'Elon' },
+  { id: 2, fname: 'Rob' },
+  { id: 3, fname: 'Tom' },
+  { id: 4, fname: 'Alex' },
+  { id: 5, fname: 'Max' },
+]
+class UserSection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: userDB.map(u => ({ ...u, isSelected: false }))
+    }
+  }
+
+  setUserSelector = (newUsers) => {
+    this.setState({ users: newUsers })
+
+  }
+
+  render() {
+    const { users } = this.state;
+
+    return (
+      <section>
+        <SelectedUsers users={users} />
+        <UserList users={users} setUserSelector={this.setUserSelector} />
+      </section>
+    );
+  }
+}
+
+export default UserSection;
